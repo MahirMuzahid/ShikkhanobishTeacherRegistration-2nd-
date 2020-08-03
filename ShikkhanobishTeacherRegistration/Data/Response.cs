59738e2a335cf537bcddb386dd8b39ba4085fcd1
Mahir Muzahid <muzahid12@live.com>
2020-08-03 21:13:05 +0600

    Reg portal done. Need testing

---
+++ ShikkhanobishTeacherRegistration/Data/Response.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShikkhanobishTeacherRegistration.Data
+{
+    public class Response
+    {
+        public string Massage { get; set; }
+        public int Status { get; set; }
+    }
+}
---
+++ ShikkhanobishTeacherRegistration/Data/TeacherInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShikkhanobishTeacherRegistration.Data
+{
+    public class TeacherInfo
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Institution { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Mail { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string SubjectInfo { get; set; }
+        public string response { get; set; }
+    }
+}
